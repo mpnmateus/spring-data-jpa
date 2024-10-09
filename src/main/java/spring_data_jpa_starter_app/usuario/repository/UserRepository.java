2e6238aef7a8838e52b6592faa742571d004fa1f
mateusnovo <mpnmateus@gmail.com>
2024-10-09 19:26:12 -0300

    feat: incluind metodo padrão QueryMethod na interface UserRepository

--- src/main/java/spring_data_jpa_starter_app/usuario/repository/UserRepository.java
+++ src/main/java/spring_data_jpa_starter_app/usuario/repository/UserRepository.java
@@ -3,6 +3,10 @@ package spring_data_jpa_starter_app.usuario.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import spring_data_jpa_starter_app.usuario.model.User;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    //Query Method
+    List<User> findByNameContaining(String name);
 
 }
